88b680bf74aa2eac1c9bbf57166146814c8ce5b8
Igifoshifo <matthew.abernathy@hotmail.com>
2020-08-23 18:42:44 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="appContainer">
+
+      <div className="menuWrapper">
+        Menu
+      </div>
+      <div className="leftDeck">
+        Left
+      </div>
+      <div className="playingTable">
+        Middle
+      </div>
+      <div className="rightDeck">
+        Right
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
